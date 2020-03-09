b6b518c88714d685b4e01ff84f7887059dd5bc96
Giles Cope <giles.cope@gmail.com>
2020-03-09 08:56:52 +0000

    Alpha possibly viable version of rust coverage

--- extension.js
+++ extension.js
@@ -17,29 +17,18 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.generateRustCoverage', function () {
 		// The code you place here will be executed every time your command is executed
 
-		const grcov_location = "/home/giles/.cargo/bin/grcov";
-		const is_release_mode = false;
-		const clean_command = "";
-		const build_command = "";
-		const test_command = "";
+		const config = vscode.workspace.getConfiguration('rcover');
+		const grcov_location = config.has('grcovPath') && config.get('grcovPath');
 
-		// setup environment
-		// std::env::set_var("CARGO_INCREMENTAL", "0");
+		const clean_command = config.has('cleanCommand') && config.get('cleanCommand');;
+		const test_command = config.has('testCommand') && config.get('testCommand');;
 
-		// let mut flags = String::from("-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads");
-		// let args : Vec<String> = std::env::args().collect();
-		// let is_release = args.iter().any(|arg| arg.contains("--release"));
+		let is_release = test_command.includes("--release");
 
-		// if is_release {
-		// 	flags.push_str(" -g");
-		// }
-		// std::env::set_var("RUSTFLAGS", flags);
-
-		// clean
-		const pwd = "/home/giles/p/pth/"; //vscode.workspaceFolder.uri.fsPath
+		const pwd = vscode.workspace.rootPath;
 		const cp = require('child_process')
 		const new_env = {
 			"CARGO_INCREMENTAL": "0",
@@ -47,29 +36,36 @@ function activate(context) {
 			...process.env
 		};
 
+		// Coverage won't work without debug info...
+		if (is_release && !new_env["RUSTFLAGS"].includes("-g")) {
+			new_env["RUSTFLAGS"] = new_env["RUSTFLAGS"] + " -g";
+		}
+
 		const options = {
 			cwd: pwd,
 			stdio: ["ignore", "pipe", "pipe"],
 			env: new_env
 		};
-		cp.exec('cargo clean', options, (err, stdout, stderr) => {
+		//TODO: Javascript promises were invented to avoid the below:
+		// Clean to force a rebuild
+		cp.exec(clean_command, options, (err, stdout, stderr) => {
 			console.log('stdout: ' + stdout);
 			console.log('stderr: ' + stderr);
 			if (err) {
 				console.log('error: ' + err);
 			} else {
-				vscode.window.showInformationMessage('Clean!');
-
-				cp.exec('cargo test', options, (err, stdout, stderr) => {
+				// Compile with correct flags and run tests...
+				cp.exec(test_command, options, (err, stdout, stderr) => {
 					console.log('stdout: ' + stdout);
 					console.log('stderr: ' + stderr);
 					if (err) {
-						vscode.window.showInformationMessage('Tests ran but some failed');
+						vscode.window.showInformationMessage('Coverage: Tests ran but some failed');
 					}
+					// Ensures it's there!
 					cp.exec('mkdir coverage', options, (err, stdout, stderr) => {
-						// Ensures it's there!
 
-						cp.exec('/home/giles/.cargo/bin/grcov ./target/debug -t lcov --llvm --branch --ignore-not-existing -o ./coverage/lcov-rust.info',
+						// run grcov to postprocess output into lcov info format.
+						cp.exec(grcov_location + ' ./target/debug -t lcov --llvm --branch --ignore-not-existing -o ./coverage/lcov-rust.info',
 							options, (err, stdout, stderr) => {
 								console.log('stdout: ' + stdout);
 								console.log('stderr: ' + stderr);
@@ -78,18 +74,10 @@ function activate(context) {
 									vscode.window.showInformationMessage('Could not generate coverage');
 								}
 							});
-
 					});
 				});
 			}
 		});
-
-		// run grcov to postprocess output into lcov info format.
-
-		// ensure other extension is installed.
-
-
-		// Display a message box to the user
 	});
 
 	context.subscriptions.push(disposable);
